5ab5567e00b5069cb315ba12f7066ab56a140158
Brian <blpmurphy@gmail.com>
2021-05-08 14:20:42 +0100

    Handle check for no value entered in check input

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+/*
+Tests
+console.log(document.querySelector('.message').textContent);
+// Change text content
+document.querySelector('.message').textContent = 'Correct Number!🥳';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 15;
+console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = `⛔ No number!`;
+  }
+});
