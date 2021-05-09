2982a76f0899d0f3a69d510239a923b96ef36082
Brian <blpmurphy@gmail.com>
2021-05-09 13:15:51 +0100

    Add highscore functionality and add values for out of range numbers

--- script.js
+++ script.js
@@ -15,13 +15,14 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   //   No Input
-  if (!guess) {
+  if (guess <= 0 || guess > 20) {
     document.querySelector('.message').textContent = `⛔ No number!`;
 
     // When correct number guesses
@@ -30,6 +31,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+    }
+    document.querySelector('.highscore').textContent = highscore;
 
     // When number too high
   } else if (guess > secretNumber) {
