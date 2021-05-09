92254ef737c5e5862d7b49da3ad8e37b014eb788
Brian <blpmurphy@gmail.com>
2021-05-09 12:54:46 +0100

    Add game restart functionality

--- script.js
+++ script.js
@@ -13,10 +13,8 @@ document.querySelector('.guess').value = 15;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -31,6 +29,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = `🏆 Correct Number!`;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
 
     // When number too high
   } else if (guess > secretNumber) {
@@ -53,4 +52,15 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = `Start guessing...`;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
